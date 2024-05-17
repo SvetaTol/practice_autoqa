f41deae45cfc009c0a84d981a2c55488ed91baf1
Svitlana Tolpinska <sveta.tolpinskaya@gmail.com>
2024-05-18 00:01:38 +0200

    Add request with checking  status code

--- tests/api/test_http.py
+++ tests/api/test_http.py
@@ -4,9 +4,23 @@ import requests
 @pytest.mark.http
 def test_first_request():
     r = requests.get('https://api.github.com/zen')
-    print(r.text)
+    print(f"Response is {r.text}")
 
 @pytest.mark.http
 def test_second_request():
     r = requests.get('https://api.github.com/users/defunkt')
-    print(f"Response is {r.text}")
+    body = r.json()
+    headers = r.headers
+
+    assert body['name'] == 'Chris Wanstrath'
+    assert r.status_code == 200
+    assert headers['Server'] == 'GitHub.com'
+
+    # print(f"Response Body is {r.json()}")
+    # print(f"Response Status code is {r.status_code}")
+    # print(f"Response Headers are {r.headers}")
+@pytest.mark.http
+def test_status_code_request():
+    r = requests.get('https://api.github.com/users/svitlana_tolpinska')
+    
+    assert r.status_code == 404
