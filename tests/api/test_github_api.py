ad5f29fd9b3684e3808b3580f49fb58bd204eaf2
Svitlana Tolpinska <sveta.tolpinskaya@gmail.com>
2024-06-14 21:43:45 +0200

    Add found repo

--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -12,3 +12,22 @@ def test_user_not_exists(github_api):
     r = github_api.get_user('svitlana_tolpinska')
     assert r['message'] == 'Not Found'
 
+
+@pytest.mark.api
+def test_repo_can_be_found(github_api):
+    r = github_api.search_repo('become-qa-auto')
+    assert r['total_count'] == 58
+    assert 'become-qa-auto' in r['items'][0]['name']
+
+
+@pytest.mark.api
+def test_repo_cannot_be_found(github_api):
+    r = github_api.search_repo('svitlana_tolpinska_repo_non_exist')
+    assert r['total_count'] == 0
+
+
+@pytest.mark.api
+def test_repo_with_single_char_be_found(github_api):
+    r = github_api.search_repo('s')
+    assert r['total_count'] != 0
+    
